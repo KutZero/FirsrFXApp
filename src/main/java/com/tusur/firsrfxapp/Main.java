6dbeb93d3b94f6cf7838bc98a7257c214d392e98
KutZero <82716275+KutZero@users.noreply.github.com>
2021-12-11 18:10:34 +0700

    Создал отдельные папки в ресурсах под fxml и CSS файлы, а также для картинок. Сделал примерную верстку окна educate_module_window.fxml

--- src/main/java/com/tusur/firsrfxapp/Main.java
+++ src/main/java/com/tusur/firsrfxapp/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcome-window.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
-        stage.setTitle("Welcome window");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/tusur/firsrfxapp/fxmlFiles/educate_module_window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 680, 560);
+        stage.setTitle("Educate Module");
         stage.setScene(scene);
         stage.show();
 
