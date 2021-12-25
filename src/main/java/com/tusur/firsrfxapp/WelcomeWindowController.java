39b6ccc1e49fdecd191a848694203fb8e585c540
qslls <79620999+qslls@users.noreply.github.com>
2021-12-25 09:10:08 +0300

    Работа с БД+комментарии

--- src/main/java/com/tusur/firsrfxapp/WelcomeWindowController.java
+++ src/main/java/com/tusur/firsrfxapp/WelcomeWindowController.java
@@ -25,17 +25,23 @@ public class WelcomeWindowController extends BaseController{
     private TextArea WelcomeText;
 
     @FXML
-    void initialize() {
-        WelcomeText.setEditable(false);
+    void initialize() throws SQLException {
+       WelcomeText.setEditable(false);
+        WelcomeText.setWrapText(true);
+        BDController DBControlForModuleWindow = new BDController();
+        Connection connectDB = DBControlForModuleWindow.getConnection();
+        Statement statement = connectDB.createStatement();//функции и переменные для работы с БД
+        String Name = "придумай как вставить активного пользователя";//Имя активного пользователя
+        //Замена текста приветственного окна с учётом имени пользователя
+        WelcomeText.setText("Здравствуйте," +Name+"! Вас приветствует тренажер по усвоению компетенции Kompetenzen. Вам предоставляется возможность проверить себя на знание компетенции УК-3: Способен осуществлять социальное взаимодействие и реализовывать свою роль в команде. Начните с прохождения первичного теста, нажав на кнопку “Пройти первичный тест” ниже. После прохождения первичного теста Вы сможете воспользоваться тренажером на знание ролей и понимание социальных взаимодействий членов команды, а также пройти финальный контрольный тест. Удачи! ");
 
         GoToMainMenuBTM.setOnMouseClicked(mouseEvent -> {
-            Main.getNavigation().load("main_window.fxml").Show();
+            Main.getNavigation().load("main_window.fxml").Show();//перейти сразу в меню
         });
 
         GoToPrimaryTestBTM.setOnMouseClicked(mouseEvent -> {
-            Main.getNavigation().load("primary_test_window.fxml").Show();
+            Main.getNavigation().load("primary_test_window.fxml").Show();//начать прохождение первичного теста
         });
 
     }
-
 }
